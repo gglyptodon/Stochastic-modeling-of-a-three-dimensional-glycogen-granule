3a454b306470eee946695cb142e6eeed87b58f57
gglyptodon <janina.mass@gmail.com>
2023-01-04 09:29:06 +0100

    add nre color option to plot_structure

--- glycogen_module/glycogen_module/plotting.py
+++ glycogen_module/glycogen_module/plotting.py
@@ -5,30 +5,52 @@ import numpy as np
 from pylab import pi, plt
 logger = logging.getLogger(__name__)
 
-def plot_structure(gs: GlycogenStructure):
-        XLIST=[];YLIST=[];ZLIST=[]
 
-        for chain in gs.chains:
-            for pos in chain.glucose_positions:
-                #logger.debug(f"chain {chain.id}, glucose_positions: {chain.glucose_positions}.Pos: {pos}") 
-                XLIST.append(pos[0])
-                YLIST.append(pos[1])
-                ZLIST.append(pos[2])
+def plot_structure(gs: GlycogenStructure, colors: list[str] = ['black', 'limegreen'], figsize: tuple[int] = (14, 10), size: int = 30):
+    plot_nre_in_different_color = True if len(colors) > 1 else False
+    markeredgecolor = colors[0]
+    if plot_nre_in_different_color:
+        nre_color = colors[1]
 
+    XLIST = []
+    YLIST = []
+    ZLIST = []
 
-        XLIST,YLIST,ZLIST= GlycogenStructure.L*np.asarray(XLIST),  GlycogenStructure.L*np.asarray(YLIST), GlycogenStructure.L* np.asarray(ZLIST)
-        #ax=plt.figure().gca(projection='3d')
+    XNRE = []
+    YNRE = []
+    ZNRE = []
 
-        ax=plt.figure()
-        ax = ax.add_subplot(projection='3d')
+    for chain in gs.chains:
+        XNRE.append(GlycogenStructure.L*chain.glucose_positions[-1][0])
+        YNRE.append(GlycogenStructure.L*chain.glucose_positions[-1][1])
+        ZNRE.append(GlycogenStructure.L*chain.glucose_positions[-1][2])
+        for pos in chain.glucose_positions:
+            #logger.debug(f"chain {chain.id}, glucose_positions: {chain.glucose_positions}.Pos: {pos}")
+            XLIST.append(pos[0])
+            YLIST.append(pos[1])
+            ZLIST.append(pos[2])
 
+    XLIST, YLIST, ZLIST = GlycogenStructure.L * \
+        np.asarray(XLIST),  GlycogenStructure.L * \
+        np.asarray(YLIST), GlycogenStructure.L * np.asarray(ZLIST)
 
-        ax.plot(XLIST,YLIST,ZLIST,'o',markersize=7, markeredgewidth=0.2,markeredgecolor='black',label='glycogen 3d')
-        ax.view_init(elev=15, azim=60)
-        plt.xlabel('x [nm]',fontsize='15')
+    ax = plt.figure(figsize=figsize)
+    ax = ax.add_subplot(projection='3d')
 
-        plt.ylabel('y [nm]',fontsize='15')
-        ax.set(xlim=(-30, 30), ylim=(-30, 30),zlim=(-30,30))
+    ax.plot(XLIST, YLIST, ZLIST, 'o', markersize=7, markeredgewidth=0.2,
+            markeredgecolor=markeredgecolor, label='glycogen 3d')
 
-        plt.show()
-        ax.legend()
+    if plot_nre_in_different_color:
+        ax.plot(XNRE, YNRE, ZNRE, 'o', markersize=7, markeredgewidth=0.2,
+                color=nre_color, markeredgecolor=markeredgecolor, label='glycogen 3d')
+
+    ax.view_init(elev=15, azim=60)
+    plt.xlabel('x [nm]', fontsize='15')
+
+    plt.ylabel('y [nm]', fontsize='15')
+    ax.set(xlim=(-size, size), ylim=(-size, size), zlim=(-size, size))
+
+    ax.set_zlabel(r'z [nm]', fontsize='25', rotation=200, labelpad=20)
+
+    plt.show()
+    ax.legend()
